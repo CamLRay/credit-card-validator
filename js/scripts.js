4615fb132f297c40f77adfcb4c7ed7220858abc0
Sploob <crayiphone@gmail.com>
2022-02-07 20:46:28 -0800

    Add specs two tests and pass two tests. WIP: third test Co-Authored by: Amiya Stillwell <stillwell.amiya@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function creditCardValidator(number) {
+    const numberArray = number.replaceAll(" ", "").split("").reverse().map(function(numbers, index){
+    if(index%2 === 0){
+      return numbers*2
+    }
+    return parseInt(numbers);
+  });
+  const numberArray2 = numberArray.map(function(newNumber){
+    if(newNumber > 9){
+      return newNumber[0] + newNumber[1];
+    }
+      return newNumber;
+  }); 
+  return numberArray2;
+}; 
+
+console.log(creditCardValidator("4204 0707 8046 5320"));
+
+//.split("")
+//parsInt("277375883992") returns 277375883992
+//parsInt("27").split("") returns [2,7]
+// for(let i=15, i>=0, i-=1)
